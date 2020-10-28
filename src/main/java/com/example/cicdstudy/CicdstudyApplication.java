2740f23996612006214a66f68a28577cbba21fca
83143 <83143@LCSC03V214.corp.lgcns.com>
2020-10-28 12:48:30 +0900

    TEST

---
+++ src/main/java/com/example/cicdstudy/CicdstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicdstudy/CicdstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
